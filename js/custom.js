0f87504576dacca8ebf019c5174f744058b6c295
IshraqQureshi <ishraqqureshi99@gmail.com>
2020-05-02 00:50:33 +0500

    Last Sections

--- js/custom.js
+++ js/custom.js
@@ -46,6 +46,36 @@ $(document).ready(function(){
           ]
     });
 
+    $('.projects_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        responsive: [            
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 779,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 667,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+
     $('.popupOpen').click(function(){
 
         $('.popup_overlay').addClass('overlay_visible');
